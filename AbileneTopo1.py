8fca71b95fd026c8617e8b40948856a1917af806
NikoletaFotopoulou <72744212+NikoletaFotopoulou@users.noreply.github.com>
2025-05-13 22:22:20 +0300

    Update AbileneTopo1.py

--- AbileneTopo1.py
+++ AbileneTopo1.py
@@ -1,109 +1,80 @@
-from mininet.net import Mininet
-from mininet.node import OVSKernelSwitch, Host # No RemoteController
-from mininet.cli import CLI
-from mininet.log import setLogLevel, info
-from mininet.link import TCLink
-from mininet.topo import Topo # Import Topo
+from mininet.topo import Topo
 
-# Graph data (same as before)
-NODE_DATA = [
-    {"id": 0, "label": "NewYork"}, {"id": 1, "label": "Chicago"},
-    {"id": 2, "label": "WashingtonDC"}, {"id": 3, "label": "Seattle"},
-    {"id": 4, "label": "Sunnyvale"}, {"id": 5, "label": "LosAngeles"},
-    {"id": 6, "label": "Denver"}, {"id": 7, "label": "KansasCity"},
-    {"id": 8, "label": "Houston"}, {"id": 9, "label": "Atlanta"},
-    {"id": 10, "label": "Indianapolis"}
-]
+class AbileneTopo(Topo):
+    "Simplified Abilene Network Topology for manual OpenFlow control."
 
-EDGE_DATA = [
-    {"source": 0, "target": 1}, {"source": 0, "target": 2},
-    {"source": 1, "target": 10}, {"source": 2, "target": 9},
-    {"source": 3, "target": 4}, {"source": 3, "target": 6},
-    {"source": 4, "target": 5}, {"source": 4, "target": 6},
-    {"source": 5, "target": 8}, {"source": 6, "target": 7},
-    {"source": 7, "target": 8}, {"source": 7, "target": 10},
-    {"source": 8, "target": 9}, {"source": 9, "target": 10}
-]
+    def build(self, **_opts): # Or __init__(self, **_opts) if your class examples strictly use that
+        "Build Abilene topology."
 
-OC192_BW = 10000
-HOST_LINK_BW = 1000
+        # If using __init__, you'd call: Topo.__init__(self, **_opts)
 
-class AbileneTopoManual(Topo):
-    "Abilene Network Topology for manual flow insertion"
-
-    def build(self, **_opts):
-        switches = {}
-        hosts = {}
+        # Node ID mapping (from your GML) for reference:
+        # 0: New York
+        # 1: Chicago
+        # 2: Washington DC
+        # 3: Seattle
+        # 4: Sunnyvale
+        # 5: Los Angeles
+        # 6: Denver
+        # 7: Kansas City
+        # 8: Houston
+        # 9: Atlanta
+        # 10: Indianapolis
 
         # 1. Add Switches
-        for node_info in NODE_DATA:
-            gml_id = node_info['id']
-            label = node_info['label'].replace(" ", "")
-            switch_name = f's{gml_id}'
-            switches[gml_id] = self.addSwitch(switch_name, protocols='OpenFlow13') # Specify OF version
-            info(f"*** Adding switch: {switch_name} (for GML node {label})\n")
+        # We'll use s0, s1, ... for switch names, corresponding to GML IDs.
+        s0 = self.addSwitch('s0', protocols='OpenFlow13')  # New York
+        s1 = self.addSwitch('s1', protocols='OpenFlow13')  # Chicago
+        s2 = self.addSwitch('s2', protocols='OpenFlow13')  # Washington DC
+        s3 = self.addSwitch('s3', protocols='OpenFlow13')  # Seattle
+        s4 = self.addSwitch('s4', protocols='OpenFlow13')  # Sunnyvale
+        s5 = self.addSwitch('s5', protocols='OpenFlow13')  # Los Angeles
+        s6 = self.addSwitch('s6', protocols='OpenFlow13')  # Denver
+        s7 = self.addSwitch('s7', protocols='OpenFlow13')  # Kansas City
+        s8 = self.addSwitch('s8', protocols='OpenFlow13')  # Houston
+        s9 = self.addSwitch('s9', protocols='OpenFlow13')  # Atlanta
+        s10 = self.addSwitch('s10', protocols='OpenFlow13') # Indianapolis
 
-            # Add one Host per Switch
-            host_name = f'h{gml_id}'
-            host_ip = f'10.0.{gml_id}.1/24' # Consistent IP addressing
-            hosts[gml_id] = self.addHost(host_name, ip=host_ip, mac=f'00:00:00:00:00:{gml_id:02x}')
-            info(f"*** Adding host: {host_name} with IP {host_ip}\n")
-            self.addLink(hosts[gml_id], switches[gml_id], bw=HOST_LINK_BW)
+        # Store switches in a list for easier linking by GML ID
+        switches = [s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10]
 
-        # 2. Add inter-switch Links
-        for edge_info in EDGE_DATA:
-            s1_gml_id = edge_info['source']
-            s2_gml_id = edge_info['target']
-            s1 = switches[s1_gml_id]
-            s2 = switches[s2_gml_id]
-            self.addLink(s1, s2, bw=OC192_BW)
+        # 2. Add Hosts
+        # Adding one host per switch, named h0, h1, ...
+        # Assigning predictable IPs and MACs is helpful for manual flow rules.
+        h0 = self.addHost('h0', ip='10.0.0.1/24', mac='00:00:00:00:00:00')
+        h1 = self.addHost('h1', ip='10.0.1.1/24', mac='00:00:00:00:00:01')
+        h2 = self.addHost('h2', ip='10.0.2.1/24', mac='00:00:00:00:00:02')
+        h3 = self.addHost('h3', ip='10.0.3.1/24', mac='00:00:00:00:00:03')
+        h4 = self.addHost('h4', ip='10.0.4.1/24', mac='00:00:00:00:00:04')
+        h5 = self.addHost('h5', ip='10.0.5.1/24', mac='00:00:00:00:00:05')
+        h6 = self.addHost('h6', ip='10.0.6.1/24', mac='00:00:00:00:00:06')
+        h7 = self.addHost('h7', ip='10.0.7.1/24', mac='00:00:00:00:00:07')
+        h8 = self.addHost('h8', ip='10.0.8.1/24', mac='00:00:00:00:00:08')
+        h9 = self.addHost('h9', ip='10.0.9.1/24', mac='00:00:00:00:00:09')
+        h10 = self.addHost('h10', ip='10.0.10.1/24', mac='00:00:00:00:00:0A')
 
-def run_abilene_manual():
-    topo = AbileneTopoManual()
-    net = Mininet(
-        topo=topo,
-        switch=OVSKernelSwitch,
-        controller=None,  # !!! CRITICAL: No external or default controller !!!
-        link=TCLink,
-        autoSetMacs=False, # We set MACs in the topo now
-        autoStaticArp=False # We will handle ARP with flows
-    )
+        # Store hosts in a list for easier linking
+        hosts = [h0, h1, h2, h3, h4, h5, h6, h7, h8, h9, h10]
 
-    info('*** Starting network\n')
-    net.start()
+        # 3. Add Links: Host to Switch
+        for i in range(len(hosts)):
+            self.addLink(hosts[i], switches[i]) # e.g., h0 to s0, h1 to s1
 
-    # --- Helper to find port numbers ---
-    # This is for your information; you'll use these in the CLI commands.
-    info('*** Network Interface Information:\n')
-    for switch in net.switches:
-        info(f"Interfaces for {switch.name}:\n")
-        for intf in switch.intfList():
-            if intf.link: # Only show interfaces that are part of a link
-                 # node1 can be a host or switch, node2 can be a host or switch
-                node1 = intf.link.intf1.node
-                node2 = intf.link.intf2.node
-                # Determine which node is the "other end" of the link from this switch's perspective
-                other_node = node2 if intf.node == node1 else node1
-                info(f"  {intf.name} (Port {switch.ports[intf]}) connected to {other_node.name}\n")
-            else:
-                info(f"  {intf.name} (Port {switch.ports[intf]}) (loopback or unlinked)\n")
-        info("\n")
+        # 4. Add Links: Switch to Switch (Inter-switch links from GML)
+        self.addLink(s0, s1)   # NY - Chicago
+        self.addLink(s0, s2)   # NY - DC
+        self.addLink(s1, s10)  # Chicago - Indianapolis
+        self.addLink(s2, s9)   # DC - Atlanta
+        self.addLink(s3, s4)   # Seattle - Sunnyvale
+        self.addLink(s3, s6)   # Seattle - Denver
+        self.addLink(s4, s5)   # Sunnyvale - LA
+        self.addLink(s4, s6)   # Sunnyvale - Denver
+        self.addLink(s5, s8)   # LA - Houston
+        self.addLink(s6, s7)   # Denver - Kansas City
+        self.addLink(s7, s8)   # Kansas City - Houston
+        self.addLink(s7, s10)  # Kansas City - Indianapolis
+        self.addLink(s8, s9)   # Houston - Atlanta
+        self.addLink(s9, s10)  # Atlanta - Indianapolis
 
-    info('*** Target hosts for this exercise:\n')
-    h0 = net.get('h0')
-    h10 = net.get('h10')
-    info(f"Sender: h0 (IP: {h0.IP()}, MAC: {h0.MAC()})\n")
-    info(f"Receiver: h10 (IP: {h10.IP()}, MAC: {h10.MAC()})\n")
-
-    info('*** Path 1 (s0 -> s1 -> s10)\n')
-    info('*** Path 2 (s0 -> s2 -> s9 -> s10)\n')
-
-    info('*** Running CLI. Follow the instructions for flow insertion and traffic.\n')
-    CLI(net)
-
-    info('*** Stopping network\n')
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info')
-    run_abilene_manual()
+# This dictionary is necessary for Mininet to find your topology
+topos = {'abilenetopo': (lambda: AbileneTopo())}
