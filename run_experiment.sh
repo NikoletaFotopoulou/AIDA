643cda0cff65ad6cb46da937818b9f466dffc6a4
NikoletaFotopoulou <72744212+NikoletaFotopoulou@users.noreply.github.com>
2025-05-16 21:57:16 +0300

    Update run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -5,11 +5,18 @@ sleep 5
 
 # Set up initial flows (s0-s1-s10-s7-s8-s5)
 sudo ovs-ofctl add-flow s0 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s1 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s10 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s7 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s8 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s5 in_port=2,actions=output:1
+sudo ovs-ofctl add-flow s1 in_port=2,actions=output:3
+sudo ovs-ofctl add-flow s10 in_port=2,actions=output:3
+sudo ovs-ofctl add-flow s7 in_port=4,actions=output:3
+sudo ovs-ofctl add-flow s8 in_port=3,actions=output:2
+sudo ovs-ofctl add-flow s5 in_port=3,actions=output:1
+
+sudo ovs-ofctl add-flow s5 in_port=1,actions=output:3
+sudo ovs-ofctl add-flow s8 in_port=2,actions=output:3
+sudo ovs-ofctl add-flow s7 in_port=3,actions=output:4
+sudo ovs-ofctl add-flow s10 in_port=3,actions=output:2
+sudo ovs-ofctl add-flow s1 in_port=3,actions=output:2
+sudo ovs-ofctl add-flow s0 in_port=2,actions=output:1
 
 # Start tcpdump
 xterm -e "sudo tcpdump -i h5-eth0 -w result.pcap" &
@@ -29,10 +36,16 @@ sudo ovs-ofctl del-flows s8
 sudo ovs-ofctl del-flows s5
 
 sudo ovs-ofctl add-flow s0 in_port=1,actions=output:3
-sudo ovs-ofctl add-flow s2 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s9 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s8 in_port=1,actions=output:2
-sudo ovs-ofctl add-flow s5 in_port=2,actions=output:1
+sudo ovs-ofctl add-flow s2 in_port=2,actions=output:3
+sudo ovs-ofctl add-flow s9 in_port=2,actions=output:3
+sudo ovs-ofctl add-flow s8 in_port=4,actions=output:2
+sudo ovs-ofctl add-flow s5 in_port=3,actions=output:1
+
+sudo ovs-ofctl add-flow s5 in_port=1,actions=output:3
+sudo ovs-ofctl add-flow s8 in_port=2,actions=output:4
+sudo ovs-ofctl add-flow s9 in_port=3,actions=output:2
+sudo ovs-ofctl add-flow s2 in_port=3,actions=output:2
+sudo ovs-ofctl add-flow s0 in_port=3,actions=output:1
 
 # Wait until 60 seconds
 sleep 30
