0e2e98085b462b8ff99b6d01270afeb5ffedd345
NikoletaFotopoulou <72744212+NikoletaFotopoulou@users.noreply.github.com>
2025-05-19 23:29:02 +0300

    Update run_experiment_2.sh

--- run_experiment_2.sh
+++ run_experiment_2.sh
@@ -15,12 +15,10 @@ sudo ovs-ofctl add-flow s3 in_port=3,actions=output:1
 sleep 30
 
 # Switch path (s3-s4-s5-s8)
-sudo ovs-ofctl del-flows s0
-sudo ovs-ofctl del-flows s1
-sudo ovs-ofctl del-flows s10
+sudo ovs-ofctl del-flows s3
+sudo ovs-ofctl del-flows s6
 sudo ovs-ofctl del-flows s7
 sudo ovs-ofctl del-flows s8
-sudo ovs-ofctl del-flows s5
 
 sudo ovs-ofctl add-flow s3 in_port=1,actions=output:2
 sudo ovs-ofctl add-flow s4 in_port=2,actions=output:3
@@ -34,5 +32,10 @@ sudo ovs-ofctl add-flow s3 in_port=2,actions=output:1
 
 # Wait until 60 seconds
 sleep 30
+
+sudo ovs-ofctl del-flows s3
+sudo ovs-ofctl del-flows s4
+sudo ovs-ofctl del-flows s5
+sudo ovs-ofctl del-flows s8
 
 echo "Experiment done"
