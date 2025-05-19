38f53fc9798d6aa7ac1f0988ff1821f3a5116566
NikoletaFotopoulou <72744212+NikoletaFotopoulou@users.noreply.github.com>
2025-05-20 01:15:14 +0300

    Update udp_times.py

--- udp_times.py
+++ udp_times.py
@@ -8,6 +8,6 @@ with open("udp_times.txt") as f:
         timestamp = float(parts[1])
 
         if timestamps and timestamp < max(timestamps):
-            print(f"Out-of-order detected at frame {frame_no}: {timestamp} < {max(timestamps)}")
+            print("Out-of-order detected at frame", frame_no, ":", timestamp, " < ", max(timestamps))
 
         timestamps.append(timestamp)
